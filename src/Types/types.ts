97d0f6a3885adf695cc37530cf522778da676e3b
Yehor Dreval <freeksons@gmail.com>
2024-07-25 21:05:28 +0200

    task-13

--- src/Types/types.ts
+++ src/Types/types.ts
@@ -26,3 +26,24 @@ export interface IQuiz {
   company_id: number;
   questions_list: TQuestion[];
 }
+
+interface QuizUser {
+  user_id: number;
+  user_email: string;
+  user_firstname: string;
+  user_lastname: string;
+  user_avatar: string;
+}
+export interface IFullQuiz {
+  quiz_id: number;
+  quiz_name: string;
+  quiz_title: string;
+  quiz_description: string;
+  quiz_frequency: number;
+  created_by: QuizUser;
+  questions_list: {
+    question_id: number;
+    question_text: string;
+    question_answers: string[];
+  }[];
+}
